f2117cf78901b38b83ccff5c4d9000495f91e485
strongwarmi <elianasc10@hotmail.com>
2017-07-12 13:28:37 -0500

    agregue process.env

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use('/', express.static('public'));
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
